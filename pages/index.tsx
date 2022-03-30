446960fe31c7e1ea81dabab2f2bbebc5d5bbcffd
Freddiiy <frederikgaller@live.dk>
2022-03-30 18:19:46 +0200

    fixed and remade some types

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,30 @@
-import {Grid, GridItem, SimpleGrid} from "@chakra-ui/react";
-import type {NextPage} from "next";
-import {useEffect, useState} from "react";
+import { Grid, GridItem, SimpleGrid } from "@chakra-ui/react";
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 import HearthstoneCard from "../components/HearthstoneCard";
-
-import {dummyCard, dummyCard2} from "../components/HearthstoneCard";
-import {IHearthstoneCard} from "../util/themes/types/hearthstone.t";
+import { dummyCard, dummyCard2 } from "../components/HearthstoneCard";
+import { HSCardsDb } from "../util/themes/tmpJsonDb/hearthstone.json";
+import {
+  IHearthstoneCard,
+  IHearthstonePage,
+} from "../util/themes/types/hearthstone.t";
 
 const Home: NextPage = () => {
-	const cards = [dummyCard, dummyCard2];
-	const [hsCards, SetHsCards] = useState<IHearthstoneCard[]>();
-	useEffect(() => {
-		function fetchCards() {}
-	}, []);
-	return (
-		<>
-			<Grid gap={4}>
-				{cards.map((card, key) => (
-					<GridItem key={key} colSpan={4}>
-						<HearthstoneCard key={key} {...card} />
-					</GridItem>
-				))}
-			</Grid>
-		</>
-	);
+  const importedCards: IHearthstonePage = HSCardsDb;
+  const cards = [dummyCard, dummyCard2, ...importedCards.cards];
+  const [hsCards, SetHsCards] = useState<IHearthstoneCard[]>();
+  useEffect(() => {
+    function fetchCards() {}
+  }, []);
+  return (
+    <>
+      <Grid gap={4} column={2}>
+        {cards.map((card, key) => (
+          <HearthstoneCard key={key} {...card} />
+        ))}
+      </Grid>
+    </>
+  );
 };
 
 export default Home;
--- util/themes/types/hearthstone.t.ts
+++ util/themes/types/hearthstone.t.ts
@@ -1,22 +1,38 @@
+export interface IHearthstonePage {
+	cards:     IHearthstoneCard[];
+	cardCount: number;
+	pageCount: number;
+	page:      number;
+}
+
 export interface IHearthstoneCard {
-	id: number;
-	collectible: number;
-	slug: string;
-	classId: number;
-	multiClassIds?: any[];
-    spellSchoolId?: number,
-	cardTypeId: number;
-	cardSetId: number;
-	rarityId: number;
-	artistName: string;
-	health?: number;
-	attack?: number;
-	manaCost: number;
-	name: string;
-	text: string;
-	image: string;
-	imageGold: string;
-	flavorText: string;
-	cropImage: string;
-	keywordIds: number[];
+	id:             number;
+	collectible:    number;
+	slug:           string;
+	classId:        number;
+	multiClassIds:  number[];
+	spellSchoolId?: number;
+	cardTypeId:     number;
+	cardSetId:      number;
+	rarityId:       number;
+	artistName:     string;
+	manaCost:       number;
+	name:           string;
+	text:           string;
+	image:          string;
+	imageGold:      string;
+	flavorText:     string;
+	cropImage:      string;
+	keywordIds?:    number[];
+	duels?:         Duels;
+	health?:        number;
+	attack?:        number;
+	copyOfCardId?:  number;
+	minionTypeId?:  number;
+	childIds?:      number[];
+}
+
+export interface Duels {
+	relevant:    boolean;
+	constructed: boolean;
 }
