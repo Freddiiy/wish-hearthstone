6262391e81ae4c6d962cf39f65c3a9dbea503f9c
Freddiiy <frederikgaller@live.dk>
2022-03-31 08:22:58 +0200

    sidebar start

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,64 +1,50 @@
 import {
-  Box,
-  Center,
-  Flex,
-  Grid,
-  GridItem,
-  HStack,
-  SimpleGrid,
-  Text,
+	Box,
+	Center,
+	Flex,
+	Grid,
+	GridItem,
+	HStack,
+	SimpleGrid,
+	Text,
 } from "@chakra-ui/react";
-import type { NextPage } from "next";
-import { useEffect, useState } from "react";
+import type {NextPage} from "next";
+import {useEffect, useState} from "react";
 import HearthstoneCard from "../components/HearthstoneCard";
-import { dummyCard, dummyCard2 } from "../components/HearthstoneCard";
-import { HSCardsDb } from "../util/themes/tmpJsonDb/hearthstone.json";
+import {dummyCard, dummyCard2} from "../components/HearthstoneCard";
+import Sidebar from "../components/Sidebar/Sidebar";
+import {HSCardsDb} from "../util/themes/tmpJsonDb/hearthstone.json";
 import {
-  IHearthstoneCard,
-  IHearthstonePage,
+	IHearthstoneCard,
+	IHearthstonePage,
 } from "../util/themes/types/hearthstone.t";
 
 const Home: NextPage = () => {
-  const importedCards: IHearthstonePage = HSCardsDb;
-  const cards = [dummyCard, dummyCard2, ...importedCards.cards];
-  const [hsCards, SetHsCards] = useState<IHearthstoneCard[]>();
-  useEffect(() => {
-    function fetchCards() {}
-  }, []);
-  return (
-    <>
-      <HStack>
-        <Box
-          width={"16rem"}
-          height={"100vh"}
-          bgColor={"white"}
-          position={"fixed"}
-        >
-          <Flex justifyContent={"center"}>
-            <Text color={"white"} fontSize={"2xl"}>
-              Dillermand
-            </Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Grid
-            templateColumns={{
-              base: "repeat(2, 1fr)",
-              sm: "repeat(3, 1fr)",
-              md: "repeat(4, 1fr)",
-              lg: "repeat(5, 1fr)",
-              xl: "repeat(7, 1fr)",
-            }}
-            gap={4}
-          >
-            {cards.map((card, key) => (
-              <HearthstoneCard key={key} {...card} />
-            ))}
-          </Grid>
-        </Box>
-      </HStack>
-    </>
-  );
+	const importedCards: IHearthstonePage = HSCardsDb;
+	const cards = [dummyCard, dummyCard2, ...importedCards.cards];
+	const [hsCards, SetHsCards] = useState<IHearthstoneCard[]>();
+	useEffect(() => {
+		function fetchCards() {}
+	}, []);
+	return (
+		<>
+			<Sidebar />
+			<Grid
+				templateColumns={{
+					base: "repeat(2, 1fr)",
+					sm: "repeat(2, 1fr)",
+					md: "repeat(3, 1fr)",
+					lg: "repeat(4, 1fr)",
+					xl: "repeat(5, 1fr)",
+				}}
+				gap={4}
+			>
+				{cards.map((card, key) => (
+					<HearthstoneCard key={key} {...card} />
+				))}
+			</Grid>
+		</>
+	);
 };
 
 export default Home;
